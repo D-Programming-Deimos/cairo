42b9ae787f4c9840af222235755f78e13d15f7a5
James Miller <bladeon@gmail.com>
2012-04-27 18:40:31 +1200

    Initial commit

---
+++ deimos/cairo/features.d
@@ -0,0 +1,28 @@
+module deimos.cairo.features;
+
+enum CairoHasFcFont = true;
+enum CairoHasFtFont = true;
+enum CairoHasGobjectFunctions = true;
+enum CairoHasImageSurface = true;
+enum CairoHasMimeSurface = true;
+enum CairoHasObserverSurface = true;
+enum CairoHasPdfSurface = true;
+enum CairoHasPngFunctions = true;
+enum CairoHasPsSurface = true;
+enum CairoHasRecordingSurface = true;
+enum CairoHasScriptSurface = true;
+enum CairoHasSvgSurface = true;
+enum CairoHasTeeSurface = true;
+enum CairoHasUserFont = true;
+enum CairoHasXcbShmFunctions = true;
+enum CairoHasXcbSurface = true;
+enum CairoHasXlibSurface = true;
+enum CairoHasXlibXrenderSurface = true;
+
+enum CairoHasEglFunctions = false;
+enum CairoHasGlxFunctions = false;
+enum CairoHasQuartzFont = false;
+enum CairoHasQuartzSurface = false;
+enum CairoHasWglFunctions = false;
+enum CairoHasWin32Font = false;
+enum CairoHasWin32Surface = false;
